aa96e557a46868e35b74d6b56382609c22fb4bae
Shem <smagnezi8@gmail.com>
2025-03-05 16:51:56 +0200

    read env vars properly

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-ENGINE_BASE_URL=https://engine.wilco.gg
-WILCO_ID=620ba777a0cccde6931ad14d
-# Read WILCO_ID from .wilco file
-#WILCO_ID="`cat .wilco`"
+WILCO_ID="`cat .wilco`"
 ENGINE_EVENT_ENDPOINT="${ENGINE_BASE_URL}/users/${WILCO_ID}/event"
 ENGINE_WILCO_AI_CONFIG="${ENGINE_BASE_URL}/users/${WILCO_ID}/wilcoAiConfig"
 CODESPACE_BACKEND_HOST=$(curl -s "${ENGINE_BASE_URL}/api/v1/codespace/backendHost?codespaceName=${CODESPACE_NAME}&portForwarding=${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}" | jq -r '.codespaceBackendHost')
